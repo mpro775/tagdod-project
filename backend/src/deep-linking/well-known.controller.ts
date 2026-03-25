2c17e3d5cfc56ea7594abb175bbfdff42e2e2db8
Muhammed Murad <eng.muhammedmurad@gmail.com>
2026-03-25 20:27:35 +0300

    feat: Add additional SHA256 certificate fingerprint to WellKnownController for enhanced security

--- backend/src/deep-linking/well-known.controller.ts
+++ backend/src/deep-linking/well-known.controller.ts
@@ -21,6 +21,7 @@ export class WellKnownController {
           sha256_cert_fingerprints: [
             '2F:2F:6B:22:72:6A:A5:33:E1:32:A1:13:37:C0:35:6B:58:C7:DB:35:B8:76:14:EB:40:10:A0:F6:6B:8B:C6:76',
             '07:A6:71:0E:FB:CF:E7:30:2C:EA:F1:E9:A2:38:D6:97:DF:B8:81:D7:E5:FA:E4:B3:9A:76:8C:5E:A9:93:7E:1D',
+            '1C:8F:D0:5C:BD:FA:40:61:A4:F0:92:50:4A:2B:3D:B9:D4:67:4B:82:A9:2A:5C:90:F2:51:AF:FD:37:2C:D5:C7',
           ],
         },
       },
